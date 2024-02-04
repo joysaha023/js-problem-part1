84f2ea0a6bbfd58b4b8c3d597f778f5c14f09e78
Joy <github.joysaha@gmail.com>
2024-02-04 10:48:32 +0600

    problem task 3 is completed

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 //////////// Task 1 /////////////////
 
-function celToFaren(cel){
-    let farenheit = (cel * 1.8) + 32;
-    return farenheit;
+// function celToFaren(cel){
+//     let farenheit = (cel * 1.8) + 32;
+//     return farenheit;
+// }
+
+// let a = 30;
+
+// const result = celToFaren(a);
+// console.log(`${a} degree celsius is equal to ${result} degree farenheit`);
+
+//////////////// Task 2 ////////////////////
+
+
+
+
+
+////////////// Task 3 ///////////////////
+function checkVowel(sentence){
+    let reg = /["a", "e", "i", "o", "u"]/gi;
+    let test = sentence.match(reg);
+    let char = test.length;
+    return char;
 }
 
-let a = 30;
-
-const result = celToFaren(a);
-console.log(`${a} degree celsius is equal to ${result} degree farenheit`);
+const a = "The quick fox jump over the lazy dog."
+const result = checkVowel(a);
+console.log("Number of the vowel is: ", result);
